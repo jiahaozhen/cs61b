47a1e640af6c5bfd979521dbefe4cafc3b5c0281
Jia <jiahaozhen389@gmail.com>
2023-12-20 10:07:27 +0800

    lab7 api check

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K extends Comparable, V> implements Map61B<K, V>{
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 
     private int size;
     private BSTNode root;
